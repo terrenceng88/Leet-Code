2bd577c0ec84e5bd711abab616cc3d14da4fe5ca
Terrence Ng <terrenceng321@gmail.com>
2019-10-25 15:32:35 -0700

    runs in 0ms

--- trees/binary_tree_preorder_traversal.cpp
+++ trees/binary_tree_preorder_traversal.cpp
@@ -14,7 +14,7 @@ Input: [1,null,2,3]
 Output: [1,2,3]
 Follow up: Recursive solution is trivial, could you do it iteratively?
 */
-
+ 
 /*------------------------- Solution -------------------------*/
 /**
  * Definition for a binary tree node.
