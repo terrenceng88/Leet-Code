6c05f65f81046b78cf5b69d29d283db270a88bd3
Terrence Ng <tng305@ucsc.edu>
2022-09-13 17:53:55 -0700

    runs in O(n) time

--- Best Practice Questions/Sequences/1. Two Sum.cpp
+++ Best Practice Questions/Sequences/1. Two Sum.cpp
@@ -13,6 +13,8 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
 
+//Runs in O(n) time using hash map
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
