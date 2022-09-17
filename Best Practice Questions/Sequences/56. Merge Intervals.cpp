99c53f1b237f565eb5301f157d93fa678029f691
Terrence Ng <tng305@ucsc.edu>
2022-09-16 23:24:55 -0700

    added question description

---
+++ Best Practice Questions/Sequences/56. Merge Intervals.cpp
@@ -0,0 +1,10 @@
+/*
+-----QUESTION-----
+Given an array of intervals where intervals[i] = [starti, endi], 
+merge all overlapping intervals, and return an array of the non-overlapping intervals 
+that cover all the intervals in the input.
+*/
+
+//Runs in O(
+
+class Solution {
