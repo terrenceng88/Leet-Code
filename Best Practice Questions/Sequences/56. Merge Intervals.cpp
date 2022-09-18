92b8a41fce41e31dff248992a99cc7db6e5eeb64
Terrence Ng <tng305@ucsc.edu>
2022-09-18 14:20:57 -0700

    runs in O(nLogn) time

--- Best Practice Questions/Sequences/56. Merge Intervals.cpp
+++ Best Practice Questions/Sequences/56. Merge Intervals.cpp
@@ -5,6 +5,32 @@ merge all overlapping intervals, and return an array of the non-overlapping inte
 that cover all the intervals in the input.
 */
 
-//Runs in O(
+//Runs in O(nLogn) + O(n) = O(nLogn) time
 
 class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size() <= 1) { //return intervals if only 1 in it because it can't overlap if only 1
+            return intervals;
+        }
+        
+        vector<vector<int>> result; //vector of intervals to be returned
+        
+        //O(nLogn)
+        sort(intervals.begin(), intervals.end()); //sort the vector of intervals in order (based on beginning number of each interval)
+        
+        result.push_back(intervals[0]); //push the first interval into the result
+        
+        //O(n)
+        for(int i = 1; i < intervals.size(); i++) { //traverse through intervals (start at index 1 because already put index 0 in the result)
+            if(result.back()[1] >= intervals[i][0]) { //if overlaps (the ending point of the last pair in result is less than the starting point of current interval)
+                result.back()[1] = max(result.back()[1], intervals[i][1]); //merge the interval (ending point of last pair is set to the ending point of current interval if it is higher than it currently is)
+            } else {
+                result.push_back(intervals[i]);
+            }
+        }
+        
+        return result;
+    }
+};
+
