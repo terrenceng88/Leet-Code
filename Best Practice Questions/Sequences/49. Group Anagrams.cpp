1af8d166c13a483871c8d0cf548821a26c274d43
Terrence Ng <tng305@ucsc.edu>
2022-09-18 23:37:45 -0700

    runs in O(n) time

--- Best Practice Questions/Sequences/49. Group Anagrams.cpp
+++ Best Practice Questions/Sequences/49. Group Anagrams.cpp
@@ -6,6 +6,27 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
 typically using all the original letters exactly once.
 */
 
-//Runs in O(
+//Runs in O(n) + O(n) = O(2n) = O(n)
 
 class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        vector<vector<string>> result; //resul;t to be returned
+        unordered_map<string, vector<string>> map; //hashmap to hold words for anagrams (sorted string is the key, original string is the value)
+        
+        //O(n)
+        for(int i = 0; i < strs.size(); i++) {
+            string word = strs[i]; //get string at index i of strs (leave unsorted)
+            sort(strs[i].begin(), strs[i].end()); //sort the string
+            map[strs[i]].push_back(word); //add unsorted string to value in the map at the key, sorted string is the key and the original string becomes the value
+        }
+        
+        //O(n)
+        for(auto i = map.begin(); i != map.end(); i++) { //traverse through the map
+            result.push_back(i -> second); //add the grouped strings to the result vector (value of the key in the map, that's why "i -> second" is used)
+        }
+        
+        return result;
+    }
+};
