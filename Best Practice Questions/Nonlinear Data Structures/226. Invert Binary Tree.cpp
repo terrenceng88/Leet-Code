6d178c2c62c761abee71df7bfc2fe9888e4cc867
Terrence Ng <tng305@ucsc.edu>
2022-11-09 14:51:20 -0800

    made file

---
+++ Best Practice Questions/Nonlinear Data Structures/226. Invert Binary Tree.cpp
@@ -0,0 +1,24 @@
+/*
+Given the root of a binary tree, invert the tree, and return its root.
+*/
+
+
+//Runs in O(
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        
+    }
+};
