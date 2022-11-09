c47f3600f344779740eb35bd60c366f66417739b
Terrence Ng <tng305@ucsc.edu>
2022-11-09 14:48:05 -0800

    runs in O(n^2) time

--- Best Practice Questions/Data Structures/76. Minimum Window Substring.cpp
+++ Best Practice Questions/Data Structures/76. Minimum Window Substring.cpp
@@ -26,11 +26,62 @@ Since the largest window of s only has one 'a', return empty string.
 */
 
 
-//Runs in O(
+//Runs in O(n^2) time complexity (looked at answer)
 
 class Solution {
 public:
+    /*
     string minWindow(string s, string t) {
+        unordered_map<char, int> hashmap; //key = letter, value = how many times it shows up in t
+        int left = 0; //pointer 1 for sliding window (goes first)
+        int right = 0; //pointer 2 for sliding window (goes second)
+        int head = 0; //head of the substring to be returned
+        int size = INT_MAX; //size of the smallest substring so far
+        int letters = t.size(); //number of letters in t that are found in s
         
+        //fill hashmap with letters in t and how many times they show up
+        for(int i = 0; i < t.size(); i++) { 
+            hashmap[t[i]]++;
+        }
+        
+        while(right < s.size()) { //iterate right pointer all the way to the end
+            if(hashmap[s[right]] > 0) {
+                letters--;
+                hashmap[s[right]]--;
+                right++;
+                while(letters == 0) {
+                    if(right - left < size)  {
+                        size = right - left;
+                        head = left;
+                    }
+                    if(hashmap[s[left]] == 0)  {
+                        letters++;
+                        hashmap[s[left]]++;
+                        s[left]++;
+                    }
+                }
+            }
+        }
+        
+        if(size == INT_MAX) {
+            return "";
+        } else {
+            return s.substr(head, size);
+        }
+    }
+    */
+    
+    string minWindow(string s, string t) {
+        vector<int> map(128,0);
+        for(auto c: t) map[c]++;
+        int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
+        while(end<s.size()){
+            if(map[s[end++]]-->0) counter--; //in t
+            while(counter==0){ //valid
+                if(end-begin<d)  d=end-(head=begin);
+                if(map[s[begin++]]++==0) counter++;  //make it invalid
+            }  
+        }
+        return d==INT_MAX? "":s.substr(head, d);
     }
 };
