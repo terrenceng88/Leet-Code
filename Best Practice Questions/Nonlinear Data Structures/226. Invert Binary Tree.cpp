b671bc1957493b0d2009cb474f3f9b0f1608ae5f
Terrence Ng <tng305@ucsc.edu>
2022-11-10 17:31:33 -0800

    runs in O(V + E) time comoplexity using DFS

--- Best Practice Questions/Nonlinear Data Structures/226. Invert Binary Tree.cpp
+++ Best Practice Questions/Nonlinear Data Structures/226. Invert Binary Tree.cpp
@@ -3,7 +3,7 @@ Given the root of a binary tree, invert the tree, and return its root.
 */
 
 
-//Runs in O(
+//Runs in O(V + E) using DFS
 
 /**
  * Definition for a binary tree node.
@@ -19,6 +19,13 @@ Given the root of a binary tree, invert the tree, and return its root.
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL) { //base case for recursive call
+            return root;
+        }
         
+        TreeNode* temp = invertTree(root->left); //recursively call on left node
+        root->left = invertTree(root->right); //recursively call on right node
+        root->right = temp; //final switch between left and right nodes
+        return root;
     }
 };
