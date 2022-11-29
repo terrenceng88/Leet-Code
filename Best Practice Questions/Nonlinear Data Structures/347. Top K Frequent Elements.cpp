c5618884f2253219ed637623421654c669354fbf
Terrence Ng <tng305@ucsc.edu>
2022-11-29 15:48:04 -0800

    made file

---
+++ Best Practice Questions/Nonlinear Data Structures/347. Top K Frequent Elements.cpp
@@ -0,0 +1,13 @@
+/*
+Given an integer array nums and an integer k, return the k most frequent elements.
+You may return the answer in any order.
+*/
+
+//Runs in O(
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+    }
+};
