7d75b927b0d1a4ee83584cec169e21dea754f74f
Terrence Ng <tng305@ucsc.edu>
2022-11-29 15:46:47 -0800

    runs in O(n) time complexity

--- Best Practice Questions/Nonlinear Data Structures/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Best Practice Questions/Nonlinear Data Structures/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -4,7 +4,7 @@ the preorder traversal of a binary tree and inorder is the inorder traversal of 
 construct and return the binary tree.
 */
 
-//Runs in O(
+//Runs in O(n) time complexity
 
 /**
  * Definition for a binary tree node.
@@ -19,19 +19,37 @@ construct and return the binary tree.
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        /*
-        Pre-order Traversal
-        - visit the root first
-        - traverse left subtree
-        - traverse right subtree
-        
-        In-order Traversal
-        - traerse left subtree
-        - visit root
-        - traverse right subtree
-        */
+    /*
+    Pre-order Traversal
+    - visit the root first
+    - traverse left subtree
+    - traverse right subtree
 
+    In-order Traversal
+    - traerse left subtree
+    - visit root
+    - traverse right subtree
+    */
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int rootIdx = 0;
+        return build(preorder, inorder, rootIdx, 0, inorder.size()-1);
+    }
+    
+    //reccursive helper function to build the tree
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int& rootIdx, int left, int right) {
+        if (left > right) {
+            return NULL;
+        }
+        int pivot = left;  // find the root from inorder
+        while(inorder[pivot] != preorder[rootIdx]) { //keep moving the pivot until it reaches the root index
+            pivot++;
+        }
         
+        rootIdx++; 
+        TreeNode* newNode = new TreeNode(inorder[pivot]);
+        newNode->left = build(preorder, inorder, rootIdx, left, pivot-1); //recursively call on the left side
+        newNode->right = build(preorder, inorder, rootIdx, pivot+1, right); //recursively call on the right side
+        return newNode;
     }
 };
