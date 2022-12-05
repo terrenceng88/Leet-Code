b99dc6b42966101b5425db6c896c713b14ea069d
Terrence Ng <tng305@ucsc.edu>
2022-12-05 15:16:07 -0800

    added the definition of deep clone

--- Best Practice Questions/Nonlinear Data Structures/133. Clone Graph.cpp
+++ Best Practice Questions/Nonlinear Data Structures/133. Clone Graph.cpp
@@ -11,6 +11,25 @@ class Node {
 }
 */
 
+/*
+Deep Clone -------------------------------------------------------------------------
+An alternative is a deep copy, meaning that fields are dereferenced: 
+rather than references to objects being copied, new copy objects are 
+created for any referenced objects, and references to these placed in B. 
+The result is different from the result a shallow copy gives in that the 
+objects referenced by the copy B are distinct from those referenced by A, 
+and independent. Deep copies are more expensive, due to needing to create 
+additional objects, and can be substantially more complicated, due to 
+references possibly forming a complicated graph.
+
+Deep copy is a process in which the copying process occurs recursively. 
+It means first constructing a new collection object and then recursively 
+populating it with copies of the child objects found in the original. 
+In case of deep copy, a copy of object is copied in other object. 
+It means that any changes made to a copy of object do not reflect in the original object. 
+In python, this is implemented using “deep copy()” function.
+*/
+
 //Runs in O(
 
 /*
