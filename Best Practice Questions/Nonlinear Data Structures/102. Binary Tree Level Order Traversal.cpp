068c775601e16e4d7f4b983659a2428841a80163
Terrence Ng <tng305@ucsc.edu>
2023-04-21 18:37:35 -0700

    made file

---
+++ Best Practice Questions/Nonlinear Data Structures/102. Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,27 @@
+/*
+Given the root of a binary tree, return the level order traversal of its nodes' values. 
+(i.e., from left to right, level by level).
+*/
+
+
+
+//Runs in O(
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        
+    }
+};
