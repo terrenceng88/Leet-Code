5864d341b05c83e064f2a94ada5bee2e7decb1ec
Terrence Ng <tng305@ucsc.edu>
2023-04-24 21:01:13 -0700

    runs in O(n) time complexity using BFS

--- Best Practice Questions/Nonlinear Data Structures/102. Binary Tree Level Order Traversal.cpp
+++ Best Practice Questions/Nonlinear Data Structures/102. Binary Tree Level Order Traversal.cpp
@@ -5,7 +5,7 @@ Given the root of a binary tree, return the level order traversal of its nodes' 
 
 
 
-//Runs in O(
+//Runs in O(n) time complexity using BFS
 
 
 /**
@@ -22,6 +22,24 @@ Given the root of a binary tree, return the level order traversal of its nodes' 
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        
+        //need BFS for this
+        vector<vector<int>> answer; //vector of vectors to hold each level of tree nodes from left to right 
+        if(!root) return answer; //if tree empty, return
+        queue<TreeNode*> q; //queue to 
+        q.push(root); //queue to hold current level of tree
+
+        while(!q.empty()) { //continue until every level of the tree is explored
+            int size = q.size();
+            vector<int> vec; //create vector to hold nodes for this level
+            while(size--) { //check every node for this level
+                TreeNode* f = q.front(); //move through nodes from left to right
+                vec.push_back(q.front()->val);
+                q.pop();
+                if(f->left) q.push(f->left);
+                if(f->right) q.push(f->right);
+            }
+            answer.push_back(vec); //add vetor of nodes for this level to answer
+        }
+        return answer;
     }
 };
