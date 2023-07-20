6f88211d4fa043c8af3d655df3b2c021f7283183
Terrence Ng <tng305@ucsc.edu>
2023-07-19 21:14:49 -0700

    created file

---
+++ Best Practice Questions/More Data Structures/208. Implement Trie (Prefix Tree).cpp
@@ -0,0 +1,42 @@
+/*
+A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+Implement the Trie class:
+
+Trie() Initializes the trie object.
+void insert(String word) Inserts the string word into the trie.
+boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+*/
+
+
+
+//Runs in O(
+
+
+class Trie {
+public:
+    Trie() {
+        
+    }
+    
+    void insert(string word) {
+        
+    }
+    
+    bool search(string word) {
+        
+    }
+    
+    bool startsWith(string prefix) {
+        
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
---
+++ Best Practice Questions/More Data Structures/211. Design Add and Search Words Data Structure.cpp
@@ -0,0 +1,36 @@
+/*
+Design a data structure that supports adding new words and finding if a string matches any previously added string.
+
+Implement the WordDictionary class:
+
+WordDictionary() Initializes the object.
+void addWord(word) Adds word to the data structure, it can be matched later.
+bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+*/
+
+
+
+//Runs in O(
+
+
+class WordDictionary {
+public:
+    WordDictionary() {
+        
+    }
+    
+    void addWord(string word) {
+        
+    }
+    
+    bool search(string word) {
+        
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
