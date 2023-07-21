22ac78c42a544a535313ed554060b22fd9f69039
Terrence Ng <tng305@ucsc.edu>
2023-07-20 22:25:53 -0700

    runs in O(n) time complexity using trie traversal (n = length of string being searched)

--- Best Practice Questions/More Data Structures/208. Implement Trie (Prefix Tree).cpp
+++ Best Practice Questions/More Data Structures/208. Implement Trie (Prefix Tree).cpp
@@ -11,27 +11,75 @@ boolean startsWith(String prefix) Returns true if there is a previously inserted
 
 
 
-//Runs in O(
+//runs in O(n) time complexity using trie traversal (n = length of string being searched)
+
+
 
 
 class Trie {
 public:
+    struct trieNode {
+        bool isEndOfWord; //marks end of a word in trie
+        trieNode* children[26]; //each letter of the alphabet
+    };
+    
+    trieNode* getNode() { //creates a new Trie node
+        trieNode* newNode = new trieNode();
+        newNode->isEndOfWord = false; //not end of word because node was just created
+        for(int i = 0; i < 26; i++) {
+            newNode->children[i] = NULL;
+        }
+        return newNode;
+    }
+
+
+    trieNode* root;
+
+
     Trie() {
-        
+        root = getNode();
     }
     
     void insert(string word) {
-        
+        trieNode* crawler = root; //used to traverse trie
+        for(char chr: word) {
+            int index = chr - 'a'; //get the current letter's place in the alphabet
+            if(crawler->children[index] == NULL) { //if letter doesn't exist for this prefix yet
+                crawler->children[index] = getNode();
+            }
+            crawler = crawler->children[index]; //move to next node in trie and letter in word
+        }
+        crawler->isEndOfWord = true; //done adding this word
     }
     
     bool search(string word) {
-        
+        trieNode* crawler = root; //used to traverse trie
+        for(char chr: word) {
+            int index = chr - 'a'; //get the current letter's place in the alphabet
+            if(crawler->children[index] == NULL) { //trie does not contain letter in word
+                return false; //word doesn't exist in trie
+            }
+            crawler = crawler->children[index];//move to next node in trie and letter in word
+        }
+        if(crawler->isEndOfWord) { //word exists in trie if all letters exist and it has been added before
+            return true;
+        }
+        return false; //word was not inserted before
     }
     
     bool startsWith(string prefix) {
-        
+        trieNode* crawler = root;
+        for(char chr: prefix) {
+            int index = chr - 'a'; 
+            if(crawler->children[index] == NULL) {
+                return false;
+            }
+            crawler = crawler->children[index]; //move to next node in trie and letter in word
+        }
+        return true;
     }
 };
+
 
 /**
  * Your Trie object will be instantiated and called as such:
@@ -40,3 +88,5 @@ public:
  * bool param_2 = obj->search(word);
  * bool param_3 = obj->startsWith(prefix);
  */
+
+
