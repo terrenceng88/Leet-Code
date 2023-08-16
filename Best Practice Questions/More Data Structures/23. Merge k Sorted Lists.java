4e10560b60841bf05a94f541b837fe83fe630a82
Terrence Ng <tng305@ucsc.edu>
2023-08-15 21:37:54 -0700

    created file

---
+++ Best Practice Questions/More Data Structures/23. Merge k Sorted Lists.java
@@ -0,0 +1,26 @@
+/*
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+*/
+
+
+//runs in O(
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+    }
+}
